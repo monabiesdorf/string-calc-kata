51822867c07487ae0ea0de1edd423ba46b6247d3
monabiesdorf <mona.biesdorf@gmx.de>
2021-07-05 19:24:32 +0200

    added negatives and ignoreThousands

--- src/string-calc.js
+++ src/string-calc.js
@@ -1,21 +1,36 @@
-function stringCalc(numbers){
-    
-    const stringList = numbers.split( /\D+/g)
-    const numList = stringList.map((x) => parseInt(x, 10))
+function stringCalc(numbers) {
 
-    console.log(stringList)
-    
+    const stringList = numbers.split(/[^\d+-]+/g)
+    const negatives = []
+    const numList = stringList.map((x) => {
+        let number = parseInt(x, 10)
+        if (number > 1000) {
+            number = 0
+        }
+        
+        return number
+    }
+    )
+    numList.map ((x) => {
+        if (x < 0) {
+        negatives.push(x)    
+        }
+    })
 
-    if(numbers === "") 
-    {
+    if (negatives.length > 0) {
+        return "negatives not allowed"
+    }
+
+    if (numbers === "") {
         return 0
     }
-    let sum = 0;
+
+    let sum = 0
     numList.forEach((x) => {
         sum += x
     })
 
-        return sum
+    return sum
 }
 
 
--- src/string-calc.spec.js
+++ src/string-calc.spec.js
@@ -1,10 +1,12 @@
 const {stringCalc} = require ("./string-calc");
 
 describe("stringCalc", () => {
+    // empty string
     it ("should return 0 on empty string", () => {
         expect(stringCalc("")).toBe(0)
     }) 
 
+    //base 
     it ("should return 1", () => {
         expect(stringCalc("1")).toBe(1)
     })
@@ -13,6 +15,7 @@ describe("stringCalc", () => {
         expect(stringCalc("1,2")).toBe(3)
     })
 
+    //multiple
     it ("should return 6 on '1,2,3' ", () => {
         expect(stringCalc("1,2,3")).toBe(6)
     })
@@ -20,18 +23,41 @@ describe("stringCalc", () => {
     it ("should return 22 on '4,5,6,7' ", () => {
         expect(stringCalc("4,5,6,7")).toBe(22)
     })
-
+    
+    //newline
     it ("should return 6 on '1\n2,3' ", () => {
         expect(stringCalc("1\n2,3")).toBe(6)
     })
-
-    it ("should return 3 on '1;2' ", () => {
+    
+    //delimiter
+    it ("should return 3 on '1;2'", () => {
         expect(stringCalc("1;2")).toBe(3)
     })
-
+    
     it ("should return 6 on '1;2/3' ", () => {
         expect(stringCalc("1;2/3")).toBe(6)
     })
 
+    //ignoreThousands
+    it ("should return 92 on '90,2' ", () => {
+        expect(stringCalc("90,2")).toBe(92)
+    })
+    
+    it ("should return 902 on '900,2' ", () => {
+        expect(stringCalc("900,2")).toBe(902)
+    })
+
+    it ("should return 1002 on '1000,2' ", () => {
+        expect(stringCalc("1000,2")).toBe(1002)
+    })
+
+    it ("should return 2 on '1001,2' ", () => {       
+        expect(stringCalc("1001,2")).toBe(2)
+    })
+    
+    //negatives
+    it("should throw error on negative number", () => {
+        expect(stringCalc("-1,5")).toEqual("negatives not allowed")
+    })
 })
 
